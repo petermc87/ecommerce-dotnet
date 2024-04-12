fa8205cf8bcf610960081a2193e815b7b022ce12
petermc87 <peter.mcgibney@gmail.com>
2024-04-11 22:21:20 -0400

    feat: category table in models

---
+++ Controllers/Category.cs
@@ -0,0 +1,11 @@
+namespace MyMvcApp.Models
+{
+    // Table class.
+    public class Category 
+    {
+        // Columns and HTTP verbs.
+        public int Id { get; set; }
+        public string MyProperty { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
