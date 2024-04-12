899d309617530fda56157adff58d65bca4f68863
petermc87 <peter.mcgibney@gmail.com>
2024-04-11 22:25:11 -0400

    feat: finish category

--- Controllers/Category.cs
+++ Controllers/Category.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyMvcApp.Models
 {
     // Table class.
     public class Category 
     {
         // Columns and HTTP verbs.
+        [Key] // <--- Primary key recognizes the line below this.
         public int Id { get; set; }
-        public string MyProperty { get; set; }
+        [Required] 
+        public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
 }
