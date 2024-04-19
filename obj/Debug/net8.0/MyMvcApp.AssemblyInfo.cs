f76aa2a2e941d057471b27f52ecd48971720d982
petermc87 <peter.mcgibney@gmail.com>
2024-04-18 22:47:41 -0400

    refactor: update migration files

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a41e343e065f83a7ee06eebd2058eace8eaed8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4826aba3dcfb44b7960192d47ca390d0b8954ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
