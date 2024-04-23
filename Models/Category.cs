5e538131f261460883241d90b6427e60643afd4b
petermc87 <peter.mcgibney@gmail.com>
2024-04-22 21:20:57 -0400

    chore: finish Category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,5 +3,7 @@ namespace Microsoft.Models
     public class Category 
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder{ get; set; }
     }
 }
