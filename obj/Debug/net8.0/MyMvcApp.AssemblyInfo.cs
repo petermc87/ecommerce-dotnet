2512b300cdd904f7f312b4346c89f1b6b13d8fc1
petermc87 <peter.mcgibney@gmail.com>
2024-04-24 23:05:51 -0400

    chore: update migrations

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245ff4f196bcab1012bffacb95f020c69f28a293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fdfb7346658e38781f16fba8510b18747f1d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
